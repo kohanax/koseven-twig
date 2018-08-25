c1d0de299a16987a619e644d1602411a05e0d325
Tongam Meyer Rogatman Tampubolon <tongam.tampubolon@gmail.com>
2018-08-25 10:31:57 +0700

    Interface 'Twig_LoaderInterface' not found
    Display error message:
    ErrorException [ Fatal Error ]: Interface 'Twig_LoaderInterface' not found
    
    Add a few lines from base of kohana framework.

--- init.php
+++ init.php
@@ -1,4 +1,8 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
+
+define('TWIGPATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+include Kohana::find_file('vendor', 'autoload');
 
 // Prepare twig environment
 Twig::init();
